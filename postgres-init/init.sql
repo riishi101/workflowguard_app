3868445ddc10c2df0d153b312ec191ae0ac2531c
riishi101 <raijung101@gmail.com>
2025-09-23 16:58:04 +0530

    Migrate to Google Cloud Run - Remove VPS files and add GCP configuration

--- postgres-init/init.sql
+++
@@ -1,8 +0,0 @@
--- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS workflowguard;
-
--- Connect to the database
-\c workflowguard;
-
--- Enable required extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
