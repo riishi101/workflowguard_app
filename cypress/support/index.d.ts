fd2f744aa469dc8356c325810fd84ab4bf4f5d79
riishi101 <raijung101@gmail.com>
2025-09-24 17:07:37 +0530

    Cleanup: Remove test files and dependencies, organize documentation

--- cypress/support/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-declare namespace Cypress {
-  interface Chainable<Subject = any> {
-    login(email: string, password: string): Chainable<void>
-    selectWorkflow(workflowIndex: number): Chainable<void>
-    startProtection(): Chainable<void>
-  }
-}
