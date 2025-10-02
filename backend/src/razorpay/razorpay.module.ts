29d7073be718966b743e414af9a873495cf74d7f
riishi101 <raijung101@gmail.com>
2025-10-02 19:42:09 +0530

    Fix RazorpayConfigController import path in RazorpayModule

--- backend/src/razorpay/razorpay.module.ts
+++ backend/src/razorpay/razorpay.module.ts
@@ -2,7 +2,7 @@ import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RazorpayService } from './razorpay.service';
 import { RazorpayController } from './razorpay.controller';
-import { RazorpayConfigController } from './razorpay-config.controller';
+import { RazorpayConfigController } from '../controllers/razorpay-config.controller';
 import { SubscriptionModule } from '../subscription/subscription.module';
 
 @Module({
